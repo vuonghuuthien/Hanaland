49abc61ce3c9ddeb41e29ab5132ff084e26fb113
vuonghuuthien <vuonghuuthientk@gmail.com>
2019-10-12 01:29:01 +0700

    Done Language and Menu Navigation Bar

--- Public/js/script.js
+++ Public/js/script.js
@@ -1,61 +1,26 @@
-var languageType = "vn";
-function language(languageType_new, href) {
-    if (languageType_new !== languageType) {
-        if (languageType_new === "vn") {
-            // lang
-            $(".info_language").html("LANGUAGE");
-            $( ".en" ).removeClass( "active" );
-            $( ".vn" ).addClass( "active" );
-            // menu
-            $(".home").html("TRANG CHỦ");
-            $(".login").html("ĐĂNG NHẬP");
-            $(".signup").html("ĐĂNG KÝ");
-            $(".slogan").html("Sàn giao dịch bất động sản <br> Luôn nhanh và chính xác nhất");
-            // scroll
-            $(".scroll_1").html("Lời Chào&nbsp;<span>|</span>");
-            $(".scroll_2").html("Dự Án Tốt Nhất&nbsp;<span>|</span>");
-            $(".scroll_3").html("Tất Cả Dự Án&nbsp;<span>|</span>");
-            $(".scroll_4").html("Liên Hệ&nbsp;<span>|</span>");
 
-            //$.get("./index.php", {"language": $(this).attr("vn")});
-            href = href.replace("en", "vn");
-        } else { // en
-            // lang
-            $(".info_language").html("NGÔN NGỮ");
-            $( ".vn" ).removeClass( "active" );
-            $( ".en" ).addClass( "active" );
-            // menu
-            $(".home").html("HOME");
-            $(".login").html("LOG IN");
-            $(".signup").html("SIGN UP");
-            $(".slogan").html("Real estate trading floor <br> Always fast and most accurate");
-            // scroll
-            $(".scroll_1").html("Welcome&nbsp;<span>|</span>");
-            $(".scroll_2").html("The Best Project&nbsp;<span>|</span>");
-            $(".scroll_3").html("All Projects&nbsp;<span>|</span>");
-            $(".scroll_4").html("Contact&nbsp;<span>|</span>");
-            //$.get("./index.php", {"language": $(this).attr("en")});
-            href = href.replace("vn", "en");
-        }
-        languageType = languageType_new;
-        alert(href);
-        window.location = href;  
-    }
-}
-var menuType = "home";
-function menu(menuType_new) {
-    if (menuType_new !== menuType) {
-        if (menuType_new === "home") {
+$(document).ready(function(){
+    $(".vn .info_language").html("LANGUAGE");
+    // menu
+    $(".vn .home").html("TRANG CHỦ");
+    $(".vn  .login").html("ĐĂNG NHẬP");
+    $(".vn .signup").html("ĐĂNG KÝ");
+    $(".vn .slogan").html("Sàn giao dịch bất động sản <br> Luôn nhanh và chính xác nhất");
+    // scroll
+    $(".vn .scroll_1").html("Lời Chào&nbsp;<span>|</span>");
+    $(".vn .scroll_2").html("Dự Án Tốt Nhất&nbsp;<span>|</span>");
+    $(".vn .scroll_3").html("Tất Cả Dự Án&nbsp;<span>|</span>");
+    $(".vn .scroll_4").html("Liên Hệ&nbsp;<span>|</span>");
 
-        } else if (menuType === "login") { // login
-
-        } else { // signup
-
-        }
-        menuType = "." + menuType;
-        menuType_new = "." + menuType_new;
-        $(menuType).removeClass( "active" );
-        $(menuType_new).addClass( "active" );
-        menuType = menuType_new;
-    }
-}
+    $(".en .info_language").html("NGÔN NGỮ");
+    // menu
+    $(".en .home").html("HOME");
+    $(".en .login").html("LOG IN");
+    $(".en .signup").html("SIGN UP");
+    $(".en .slogan").html("Real estate trading floor <br> Always fast and most accurate");
+    // scroll
+    $(".en .scroll_1").html("Welcome&nbsp;<span>|</span>");
+    $(".en .scroll_2").html("The Best Project&nbsp;<span>|</span>");
+    $(".en .scroll_3").html("All Projects&nbsp;<span>|</span>");
+    $(".en .scroll_4").html("Contact&nbsp;<span>|</span>");
+});
--- index.php
+++ index.php
@@ -7,13 +7,14 @@
     if (!isset($_SESSION['href'])) {
         $_SESSION['href'] = 'index.php?lang=vn';
     }
-    if (isset($_GET['lang']) and  $_GET['lang'] !== $_SESSION['lang']) {
-        $_SESSION['href'] = 'index.php?lang=' . $_SESSION['lang'];
-    }
     if (!isset($_SESSION['page'])) {
         $_SESSION['page'] = 'home';
     }
-    if (isset($_GET['controller']) and  $_GET['controller'] !== $_SESSION['page']) {
+    if (isset($_GET['lang'])) {
+        $_SESSION['lang'] = $_GET['lang'];
+        $_SESSION['href'] = 'index.php?lang=' . $_GET['lang'];
+    }
+    if (isset($_GET['controller'])) {
         $_SESSION['page'] = $_GET['controller'];
     }
 
@@ -78,71 +79,35 @@
     </style>
     <title>Hanaland</title>
 </head>
-<body>
-    <?php 
-        if(isset($_GET['lang'])) {
-            $lang = $_GET['lang'];
-        } else {
-            $controller = '';
-        }
-        // Require_once file in Controller // Include vs Include_once vs Require vs Require_once
-        switch($controller) {
-            case 'users': {
-                //require_once('View/thanhvien/add_user.php');
-                echo '<link rel="stylesheet" type="text/css" href="./Public/css/users/style.css">';
-                break;
-            }
-            case 'login': {
-                echo '<link rel="stylesheet" type="text/css" href="./Public/css/login/style.css">';
-                break;
-            }
-            case 'signup': {
-                echo '<link rel="stylesheet" type="text/css" href="./Public/css/signup/style.css">';
-                break;
-            }
-            case 'home': {
-                echo '<link rel="stylesheet" type="text/css" href="./Public/css/home/style.css">';
-                break;
-            }
-            default: {
-                echo '<link rel="stylesheet" type="text/css" href="./Public/css/home/style.css">';
-                break;
-            }
-        }
-    ?>
+<body class="<?= $_SESSION['lang'] ?>">
     <div class="navigationBar">
         <div class="background"></div>
         <div class="logo"></div>
         <div class="menuNavigation">
             <ul>
-                <li><a href="<?php echo($_SESSION['href'] . '&controller=home');?>" class="home active" onclick="menu('home')">Trang Chủ</a></li>
+                <li><a href="<?php echo($_SESSION['href'] . '&controller=home');?>" class="home <?php if($_SESSION['page']=='home') { echo ('active disabled'); } ?>" onclick="menu('home')">Trang Chủ</a></li>
                 <li>
-                    <a href="<?php echo($_SESSION['href'] . '&controller=login');?>" class="login" onclick="menu('login')">Đăng Nhập</a>
+                    <a href="<?php echo($_SESSION['href'] . '&controller=login');?>" class="login <?php if($_SESSION['page']=='login') { echo ('active disabled'); } ?>" onclick="menu('login')">Đăng Nhập</a>
                     <a class="space">/</a>
-                    <a href="<?php echo($_SESSION['href'] . '&controller=signup');?>" class="signup" onclick="menu('signup')">Đăng Ký</a>
+                    <a href="<?php echo($_SESSION['href'] . '&controller=signup');?>" class="signup <?php if($_SESSION['page']=='signup') { echo ('active disabled'); } ?>" onclick="menu('signup')">Đăng Ký</a>
                 </li>
             </ul>
         </div>
         <div class="menuHamburger">
             <div class="icon"></div>
         </div>
+        <?php
+            $href = $_SESSION['href'] . '&controller=' . $_SESSION['page']; 
+        ?>
         <div class="language">
             <ul>
-                <li><div class="en" onclick="language('en', '<?php echo($_SESSION['href'] . '&controller=' . $_SESSION['page']);?>')">EN</div></li>
-                <li><div class="vn active" onclick="language('vn', '<?php echo($_SESSION['href'] . '&controller=' . $_SESSION['page']);?>')">VN</div></li>
+                <li><a class="en_language <?php if($_SESSION['lang']=='en') { echo ('active disabled'); } ?>" href="<?= str_replace("vn", "en", $href); ?>">EN</a></li>
+                <li><a class="vn_language <?php if($_SESSION['lang']=='vn') { echo ('active disabled'); } ?>" href="<?= str_replace("en", "vn", $href); ?>">VN</a></li>
             </ul>
             <ul class="info_language">LANGUAGE</ul>
         </div>
     </div>
     <?php //echo ( $msg ); ?>
-    <?php 
-        echo ( $_SESSION['lang'] ) . '<br>'; 
-        echo ($_SESSION['href'] . '<br>'); 
-        echo $_SERVER['REQUEST_URI'] . '<br>'; 
-        $pos = strpos($_SERVER['REQUEST_URI'], 'index.php');
-        echo substr($_SERVER['REQUEST_URI'], $pos)."<br>";
-
-    ?>
     <div class="navigationBar_temp"></div>
     <?php
         // Kết nối đối Database
@@ -158,35 +123,30 @@
         // Require_once file in Controller // Include vs Include_once vs Require vs Require_once
         switch($controller) {
             case 'users': {
-                $_SESSION['page'] = 'users';
                 //require_once('View/thanhvien/add_user.php');
                 require_once('Controller/users/index.php');
                 break;
             }
             case 'login': {
-                $_SESSION['page'] = 'login';
                 require_once('Controller/login/index.php');
                 break;
             }
             case 'signup': {
-                $_SESSION['page'] = 'signup';
                 require_once('Controller/signup/index.php');
                 break;
             }
             case 'home': {
-                $_SESSION['page'] = 'home';
                 require_once('Controller/home/index.php');
                 //echo '<script src="./Public/js/home/script.js"></script>';
                 break;
             }
             default: {
-                $_SESSION['page'] = 'home';
                 require_once('Controller/home/index.php');
                 break;
             }
         }
     ?>
-    <script src="./Public/js/script.php"></script>
+    <!-- <script src="./Public/js/script.php"></script> -->
     <script src="./Public/js/script.js"></script>
 </body>
 </html>
